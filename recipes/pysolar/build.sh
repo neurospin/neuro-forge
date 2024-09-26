a271fefbbf2d97d6384f2c30862774501b300880
Vincent Frouin <vincent.frouin@cea.fr>
2024-09-26 15:22:26 +0200

    FIX: DEFAULT_PATH_SOLAR

--- recipes/pysolar/build.sh
+++ recipes/pysolar/build.sh
@@ -1,6 +1,7 @@
 python -m pip install -t "$PREFIX" --no-deps --no-build-isolation --force-reinstall "$SRC_DIR"
 mkdir "$PREFIX/site-packages"
 mv $PREFIX/pysolar* $PREFIX/site-packages
+sed -i s+/neurospin/brainomics/ig_applis/bin/solar+$PREFIX/bin $PREFIX/site-packages/pysolar/info.py
 for i in $PREFIX/bin/*; do
     sed -i '1s:.*:#!/usr/bin/env python:' $i
 done
