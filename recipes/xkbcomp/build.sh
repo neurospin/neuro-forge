ed1221857ebaf05f6ba79e88d12e8fc2f5dd91b1
denisri <denis.riviere.gm@gmail.com>
2025-01-15 17:08:00 +0100

    package for xkbcomp, used by xvfb

---
+++ recipes/xkbcomp/build.sh
@@ -0,0 +1,10 @@
+export PKG_CONFIG_PATH=$CONDA_PREFIX/x86_64-conda-linux-gnu/sysroot/usr/share/pkgconfig:"$PKG_CONFIG_PATH"
+export ACLOCAL_PATH=$CONDA_PREFIX/x86_64-conda-linux-gnu/sysroot/usr/share/aclocal:"$ACLOCAL_PATH"
+export CPPFLAGS="-isystem ${CONDA_PREFIX}/include -isystem ${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/include"
+# export CPPFLAGS="-isystem /volatile/riviere/casa-distro/conda/brainvisa-6.0/src/neuro-forge/.pixi/envs/default/include"
+./autogen.sh
+./configure --prefix=$PREFIX
+echo CPPFLAGS: $CPPFLAGS
+make -j -k
+make VERBOSE=1
+make install
