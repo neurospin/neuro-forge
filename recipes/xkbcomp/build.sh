8daacf63bf7a43c45eb3fb2e7fd854484d899e94
Denis Rivière <denis.riviere.gm@gmail.com>
2025-01-21 12:31:50 +0100

    fixed PKG_CONFIG_PATH to build the xkbcomp package

--- recipes/xkbcomp/build.sh
+++ recipes/xkbcomp/build.sh
@@ -1,4 +1,4 @@
-export PKG_CONFIG_PATH=$CONDA_PREFIX/x86_64-conda-linux-gnu/sysroot/usr/share/pkgconfig:"$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH=${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/share/pkgconfig:${CONDA_PREFIX}/lib/pkgconfig:"$PKG_CONFIG_PATH"
 export ACLOCAL_PATH=$CONDA_PREFIX/x86_64-conda-linux-gnu/sysroot/usr/share/aclocal:"$ACLOCAL_PATH"
 export CPPFLAGS="-isystem ${CONDA_PREFIX}/include -isystem ${CONDA_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/include"
 # export CPPFLAGS="-isystem /volatile/riviere/casa-distro/conda/brainvisa-6.0/src/neuro-forge/.pixi/envs/default/include"
