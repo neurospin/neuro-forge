9c73a0762f429dbe6494036a0087503d24c52eeb
sapetnioc <sapetnioc@users.noreply.github.com>
2025-12-12 10:54:01 +0100

    Added soma-pytorch to force torch and torchvision installation via Pypi

---
+++ recipes/soma-pytorch/soma-pytorch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
+if [ ! -e "$CONDA_PREFIX/brainvisa-override/lib/python$PYTHON_VERSION/site-packages/torch" ]; then
+    python -m pip install --prefix "$CONDA_PREFIX/brainvisa-override" --force-reinstall --no-deps torch torchvision
+    python -m pip install --prefix "$CONDA_PREFIX/brainvisa-override" torch torchvision
+fi
+
+python_dir="$CONDA_PREFIX/brainvisa-override/lib/python$PYTHON_VERSION/site-packages"
+
+first_pythonpath_component=$(printf '%s\n' "$PYTHONPATH" | cut -d: -f1)
+
+if [ "$first_pythonpath_component" != "$python_dir" ]; then
+    export PYTHONPATH="$python_dir:$PYTHONPATH"
+fi
