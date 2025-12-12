c87cf4198daa8241372215b3b3e40413182d4d2b
sapetnioc <sapetnioc@users.noreply.github.com>
2025-12-12 11:13:21 +0100

    brainvisa-override -> soma-override

--- recipes/soma-pytorch/soma-pytorch.sh
+++ recipes/soma-pytorch/soma-pytorch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % sys.version_info[:2])'`
-if [ ! -e "$CONDA_PREFIX/brainvisa-override/lib/python$PYTHON_VERSION/site-packages/torch" ]; then
-    python -m pip install --prefix "$CONDA_PREFIX/brainvisa-override" --force-reinstall --no-deps torch torchvision
-    python -m pip install --prefix "$CONDA_PREFIX/brainvisa-override" torch torchvision
+if [ ! -e "$CONDA_PREFIX/soma-override/lib/python$PYTHON_VERSION/site-packages/torch" ]; then
+    python -m pip install --prefix "$CONDA_PREFIX/soma-override" --force-reinstall --no-deps torch torchvision
+    python -m pip install --prefix "$CONDA_PREFIX/soma-override" torch torchvision
 fi
 
-python_dir="$CONDA_PREFIX/brainvisa-override/lib/python$PYTHON_VERSION/site-packages"
+python_dir="$CONDA_PREFIX/soma-override/lib/python$PYTHON_VERSION/site-packages"
 
 first_pythonpath_component=$(printf '%s\n' "$PYTHONPATH" | cut -d: -f1)
 
